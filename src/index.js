5d4196b947e059bdee211f1f240c1f3556f0aaf7
joel-jcs <joeljcs@protonmail.com>
2024-09-15 14:56:03 -0400

    added menu page and styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "./styles.css";
 // import HomePage from './home.js';
+import MenuPage from './menu.js';
 
 const navButtons = document.querySelectorAll('button');
+navButtons.addEventListener('click', () => {
+    
+})
